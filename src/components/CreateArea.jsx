e06f557ee4a442439d27eb1e8e5bf7876124f5dd
Divyam Saini <153718864+divyam1209@users.noreply.github.com>
2025-05-17 21:00:00 +0530

    keeper-app

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+function CreateArea({ onAdd }) {
+  const [note, setNote] = useState({ title: "", content: "" });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNote((prev) => ({ ...prev, [name]: value }));
+  }
+
+  function submitNote(event) {
+    onAdd(note);
+    setNote({ title: "", content: "" });
+    event.preventDefault();
+  }
+
+  return (
+    <form>
+      <input
+        name="title"
+        onChange={handleChange}
+        value={note.title}
+        placeholder="Title"
+      />
+      <textarea
+        name="content"
+        onChange={handleChange}
+        value={note.content}
+        placeholder="Take a note..."
+        rows="3"
+      />
+      <button onClick={submitNote}>Add</button>
+    </form>
+  );
+}
+
+export default CreateArea;
---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+function Note({ title, content, onDelete, id }) {
+  return (
+    <div className="note">
+      <h1>{title}</h1>
+      <p>{content}</p>
+      <button onClick={() => onDelete(id)}>Delete</button>
+    </div>
+  );
+}
+
+export default Note;
